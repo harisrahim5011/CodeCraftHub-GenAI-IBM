41b885d66b5da4ab4baff124bfdec25478657554
harisrahim5011 <harisrahim5011@gmail.com>
2025-04-20 12:48:07 -0400

    user.controllers type error removed

--- user-service/src/controllers/user.controller.ts
+++ user-service/src/controllers/user.controller.ts
@@ -2,21 +2,31 @@ import { Request, Response } from 'express';
 import { registerUser, authenticateUser } from '../services/user.service';
 
 export const register = async (req: Request, res: Response) => {
-  try {
-    const { name, email, password } = req.body;
-    const user = await registerUser(name, email, password);
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-};
+    try {
+        const { name, email, password } = req.body;
+        const user = await registerUser(name, email, password);
+        res.status(201).json(user);
+    } catch (err) {
+        if (err instanceof Error) {
+            res.status(401).json({ error: err.message }); // ✅ Safe access
+        } else {
+            res.status(401).json({ error: 'unknow error' });
+        }
+    };
+}
 
-export const login = async (req: Request, res: Response) => {
-  try {
-    const { email, password } = req.body;
-    const user = await authenticateUser(email, password);
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(401).json({ error: err.message });
-  }
-};
+    export const login = async (req: Request, res: Response) => {
+        try {
+            const { email, password } = req.body;
+            const user = await authenticateUser(email, password);
+            res.status(200).json(user);
+        } catch (err) {
+            if (err instanceof Error) {
+                res.status(401).json({ error: err.message });
+
+            } else {
+                res.status(401).json({ error: 'unknow error' });
+
+            }
+        }
+    }
